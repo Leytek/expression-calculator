6c19d6fdaad55a50f339951c5215b29db4e80ece
Leytek <leytek@yandex.ru>
2021-08-31 00:37:52 +0300

    feat: implement expressionCalculator

--- src/index.js
+++ src/index.js
@@ -3,8 +3,66 @@ function eval() {
     return;
 }
 
+const ops = ['+', '-', '*', '/'];
+const opsPr = (c) => c === '+' || c === '-' ? 1 : 2
+
+function shuntingYard(str) {
+    let res = '', stack = [];
+    for (let i = 0; i < str.length; ++i) {
+        let c = str[i], tc;
+        if (/^\d+$/.test(c))
+            res += c;
+        else {
+            res += res[res.length - 1] !== ' ' ? ' ' : '';
+            if (c === '(')
+                stack.push(c);
+            else
+            if (c === ')')
+                while ((tc = stack.pop()) !== '(') {
+                    if(tc === undefined)
+                        throw new Error('ExpressionError: Brackets must be paired');
+                    res += tc + ' ';
+                }
+            else
+            if (ops.includes(c)) {
+                while (ops.includes(stack[stack.length - 1]) && opsPr(stack[stack.length - 1]) >= opsPr(c))
+                    res += stack.pop() + ' ';
+                stack.push(c);
+            }
+        }
+    }
+    if(stack.includes('('))
+        throw new Error('ExpressionError: Brackets must be paired');
+    res += res[res.length - 1] !== ' ' ? ' ' : '';
+    while((tc = stack.pop()) !== undefined) {
+        res += tc + ' ';}
+    return res.trim().split(' ');
+}
+
+function rPNCalc(rpn) {
+    let stack = [];
+    for (let i = 0; i < rpn.length; ++i) {
+        let c = rpn[i];
+        if (/^\d+$/.test(c))
+            stack.push(c);
+        if (ops.includes(c)) {
+            let b = stack.pop(), a = stack.pop();
+            switch (c) {
+                case '+': stack.push(parseFloat(a, 10) + parseFloat(b, 10)); break;
+                case '-': stack.push(parseFloat(a, 10) - parseFloat(b, 10)); break;
+                case '*': stack.push(parseFloat(a, 10) * parseFloat(b, 10)); break;
+                case '/':
+                    if (parseFloat(b, 10) === 0)
+                        throw new Error('TypeError: Division by zero.');
+                    stack.push(parseFloat(a, 10) / parseFloat(b, 10)); break;
+            }
+        }
+    }
+    return stack.pop();
+}
+
 function expressionCalculator(expr) {
-    // write your solution here
+    return rPNCalc(shuntingYard(expr));
 }
 
 module.exports = {
